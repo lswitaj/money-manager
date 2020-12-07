8f6067c9f7ddf6c739dfa633a66b388c16f0fe85
lswitaj <mars654@wp.pl>
2020-12-07 08:34:38 +0100

    Logging installation in the admin panel

--- app/src/main/java/com/lswitaj/portfelmanager/MainActivity.kt
+++ app/src/main/java/com/lswitaj/portfelmanager/MainActivity.kt
@@ -2,10 +2,21 @@ package com.lswitaj.portfelmanager
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.parse.Parse
+import com.parse.ParseInstallation
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Parse.initialize(
+            Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.back4app_app_id))
+                .clientKey(getString(R.string.back4app_client_key))
+                .server(getString(R.string.back4app_server_url))
+                .build()
+        )
+        //TODO(temporary - to log first event in Back4app console)
+        ParseInstallation.getCurrentInstallation().saveInBackground();
         setContentView(R.layout.activity_main)
     }
 }
