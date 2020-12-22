e2ca6cef5841f7e9654c6bd38dea8e8a1d335066
lswitaj <mars654@wp.pl>
2020-12-22 08:10:41 +0100

    Basic user login

--- app/src/main/java/com/lswitaj/moneymanager/login/LogInViewModel.kt
+++ app/src/main/java/com/lswitaj/moneymanager/login/LogInViewModel.kt
@@ -1,8 +1,10 @@
 package com.lswitaj.moneymanager.login
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.parse.ParseUser
 
 class LogInViewModel() : ViewModel() {
 
@@ -10,8 +12,21 @@ class LogInViewModel() : ViewModel() {
     val navigateToSummary: LiveData<Boolean>
         get() = _navigateToSummary
 
+    val username = MutableLiveData<String>()
+    val password = MutableLiveData<String>()
+
     fun onLogInButtonClicked() {
-        _navigateToSummary.value = true
+        //TODO(to add snackbar when the username/password empty + for exceptions from API)
+        //TODO(login to be lowercased for both login and sing up)
+        ParseUser.logInInBackground(username.value, password.value) { user, e ->
+            if (user != null) {
+                Log.w("response", user.username)
+                Log.w("response", user.username + " " + user.email)
+                _navigateToSummary.value = true
+            } else {
+                Log.w("response", e.toString())
+            }
+        }
     }
 
     fun onNavigatedToSummary() {
