d08b4957704df3a2b776b25bda2fa18ea2c4caaa
lswitaj <mars654@wp.pl>
2020-12-28 23:39:50 +0100

    Storing positions on the backend

--- app/src/main/java/com/lswitaj/moneymanager/search/SearchViewModel.kt
+++ app/src/main/java/com/lswitaj/moneymanager/search/SearchViewModel.kt
@@ -1,5 +1,6 @@
 package com.lswitaj.moneymanager.search
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -8,7 +9,12 @@ import com.lswitaj.moneymanager.data.database.SymbolsDatabaseDao
 import com.lswitaj.moneymanager.data.database.SymbolsOverview
 import com.lswitaj.moneymanager.data.network.FinnhubApi
 import com.lswitaj.moneymanager.data.network.Symbol
+import com.lswitaj.moneymanager.utils.getLastClosePrice
+import com.lswitaj.moneymanager.utils.parseErrorFormatter
+import com.parse.ParseObject
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SearchViewModel(
     val database: SymbolsDatabaseDao
@@ -25,6 +31,10 @@ class SearchViewModel(
     val navigateToSummary: LiveData<Symbol>
         get() = _navigateToSummary
 
+    private val _errorMessage = MutableLiveData<String>()
+    val errorMessage: LiveData<String>
+        get() = _errorMessage
+
     fun getAllSymbols() {
         viewModelScope.launch {
 //           try {
@@ -32,7 +42,7 @@ class SearchViewModel(
             //symbols without description or with non-letters won't be shown
             allSymbols = result
                 .filter { it.description.isNotEmpty() }
-                .filter { !it.symbol.contains(regex= Regex("""=+|\^+|#+|-+""")) }
+                .filter { !it.symbol.contains(regex = Regex("""=+|\^+|#+|-+""")) }
 
             _searchableQueryResponse.value = allSymbols
 //            } catch (e: Exception) {
@@ -48,8 +58,33 @@ class SearchViewModel(
 
     fun addNewSymbol(symbol: Symbol) {
         viewModelScope.launch {
-            database.addSymbol(SymbolsOverview(symbol.symbol))
+            //TODO(to change price to double)
+            database.addSymbol(
+                SymbolsOverview(
+                    symbol.symbol,
+                    getLastClosePrice(symbol.symbol).toBigDecimal().toPlainString()
+                )
+            )
+            addNewSymbolToBackend()
             _navigateToSummary.value = symbol
+        }
+    }
+
+    suspend fun addNewSymbolToBackend() {
+        withContext(Dispatchers.IO) {
+            val symbolOverviewParse = ParseObject("SymbolOverviewParse")
+            database.getLastSymbol().let {
+                //TODO(to extract all put expressions to the class)
+                symbolOverviewParse.put("symbolId", it.symbolId)
+                symbolOverviewParse.put("symbolName", it.symbolName)
+                symbolOverviewParse.put("lastClosePrice", it.lastClosePrice)
+            }
+
+            symbolOverviewParse.saveInBackground { e ->
+                if (e != null) {
+                    _errorMessage.value = parseErrorFormatter(e)
+                }
+            }
         }
     }
 
